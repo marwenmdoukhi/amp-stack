abea93b1eaf4f45fc406c605ef60f1969f59ee59
Mdoukhi <marwenmdoukhi@gmail.com>
2019-08-08 00:29:59 +0100

    symfony

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 wordpress_initilizing(){
 	if [[ -d "/var/www/html" ]]; then
 		cd /var/www
-		wget https://fr.wordpress.org/latest-fr_FR.zip
-		unzip latest-fr_FR.zip
-		cd wordpress
-		mv * ../html
+		composer create-project symfony/website-skeleton projet
+		chmod 777 -R projet
+		#mv -a  /var/www/html
+		mv projet /var/www/html
 		echo "[OK] Wordpress seccessfly initilized"
 	else
 		echo "[NOTICE] Wordpress alrady initilized ..."
@@ -16,7 +16,7 @@ if [[ "$1" == apache2* ]]; then
         echo ""
         echo "* SERVER INITIALIZING ... *"
         echo ""
-        wordpress_initilizing
+        #wordpress_initilizing
 		echo ""
         echo "* SERVER STARTED SUCCESSFULY *"
         echo ""
