fad41258dc3339876f275d03550d268cfdcfc885
Mdoukhi <marwenmdoukhi@gmail.com>
2019-08-08 00:24:26 +0100

    intialistion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-creadis(){
-	echo "this project is created in devops fomation"
-}
 if [[ "$1" == apache2* ]]; then
         echo ""
         echo "* SERVER INITIALIZING ... *"
         echo ""
-        creadis
 		echo ""
         echo "* SERVER STARTED SUCCESSFULY *"
         echo ""
