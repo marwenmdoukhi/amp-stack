0d6fc8bc5c35a2ec8460d545d1b3aa7b18e3377e
Mdoukhi <marwenmdoukhi@gmail.com>
2019-08-10 00:31:15 +0100

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 symfony(){
-	if [[ -d "/var/www/html" ]]; then
+	if [[ ! "$(ls -A /var/www/html)" ]]; then
 		cd /var/www
-		composer create-project symfony/website-skeleton projet
-		chmod 777 -R projet
-		#mv -a  /var/www/html
-		mv projet /var/www/html
-		echo "[OK] Wordpress seccessfly initilized"
+		composer create-project symfony/website-skeleton symfonytest
+		chmod 777 -R symfonytest
+		cd symfonytest
+		cp -a . /var/www/html
+		rm -r /var/www/symfonytest
+		echo "[OK] symfony seccessfly initilized"
 	else
-		echo "[NOTICE] Wordpress alrady initilized ..."
+		echo "[NOTICE] symfony alrady initilized ..."
 	fi
 }
 if [[ "$1" == apache2* ]]; then
